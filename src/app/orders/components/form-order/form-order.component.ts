b31e4cd8e29d3446be58fc916cdcb8cdadea188a
dnyuhm <dylanmeuriot@gmail.com>
2022-10-14 12:15:14 +0200

    Commit étape 33 : Formbuilder, reactiveformsmodule

--- src/app/orders/components/form-order/form-order.component.ts
+++ src/app/orders/components/form-order/form-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { StateOrder } from '../../enums/state-order';
 import { Order } from '../../models/order';
 
@@ -10,10 +11,22 @@ import { Order } from '../../models/order';
 export class FormOrderComponent implements OnInit {
   @Input() public init!: Order;
   public states: string[];
+  public form!: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
     this.states = Object.values(StateOrder);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      typePresta: [this.init.typePresta],
+      client: [this.init.client],
+      nbJours: [this.init.nbJours],
+      tjmHt: [this.init.tjmHt],
+      tva: [this.init.tva],
+      state: [this.init.state],
+      comment: [this.init.comment],
+      id: [this.init.id],
+    });
+  }
 }
